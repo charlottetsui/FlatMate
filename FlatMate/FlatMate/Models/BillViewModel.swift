533504783989c430afed49933b27396c9108dbfb
escoats <escoats@unc.edu>
2024-11-02 20:41:21 -0400

    Added makePayment function

--- FlatMate/FlatMate/Models/BillViewModel.swift
+++ FlatMate/FlatMate/Models/BillViewModel.swift
@@ -8,13 +8,34 @@
 
 import SwiftUI
 
-func addBill(name: String, balance: Double, numRoommates: Int) -> Bill{
+func addExpense(name: String, balance: Double, numRoommates: Int) -> Bill{
     let splitBalance: Double = balance / Double(numRoommates)
     let balanceBreakdown = Array(repeating: splitBalance, count: numRoommates)
     
     let newBill: Bill = Bill(name: name, balance: balance, balanceBreakdown: balanceBreakdown)
     
     return newBill
+}
+
+func makePayment(@Binding bill: Bill, amount: Double, userIndex: Int){
+    var userBalance: Double = bill.balanceBreakdown[userIndex]
+    
+    // Bill is already fully paid
+    if (userBalance <= 0){
+        bill.paid = true
+        return
+    }
+    
+    // User is trying to pay more than they owe
+    if (userBalance < amount){
+        bill.balanceBreakdown[userIndex] = 0.0
+        bill.balance -= userBalance
+        return
+    }
+    
+    // Regular use case
+    bill.balance -= amount
+    bill.balanceBreakdown[userIndex] = (userBalance - amount)
 }
 
 
